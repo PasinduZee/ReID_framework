336dff2ad17eb2498d7a9d9ed4b84d18c861ec0b
pasinduzee <zeeleo7@gmail.com>
2017-05-02 21:45:33 +0530

    Testing completed for all k(1:4) : forall threshold(1:10)

--- C2O.m
+++ C2O.m
@@ -1,88 +1,132 @@
 function C2O(dataset,totN)
 
-%setup viper mat files
-setup_viper 
+workDir=pwd;
 
-% loading the partitions
+%load trials
 load(fullfile(workDir,'data',sprintf('%s_trials.mat',dataset)));
 
-%  Loading features and partitions     
+%load viper features
+workDir=pwd;
+load(fullfile(workDir,'data',sprintf('%s_features.mat',dataset)));
 
 params.Iter =1;
 params.saveDir = strcat('.\Graphics\',dataset,'\'); 
-
 c2o_params.totN=totN;
-c2o_params.iter_id=7;
+c2o_params.iter_id=1;
 c2o_params.dataset='viper';
+c2o_params.idxtrain=trials(c2o_params.iter_id).labelsAtrain; %316 for viper trains
+c2o_params.non_trains=trials(c2o_params.iter_id).labelsAtest; %316 for viper non trains
 
 
-%allocate first half of images to the training
-c2o_params.idxtrain=trials(c2o_params.iter_id).labelsAtrain;
-
-%allocate second half to the non training
-c2o_params.non_trains=trials(c2o_params.iter_id).labelsAtest;
-
-
+%let 200 for randoms
 %current_probe and gallery set initializing
-c2o_params.test_start_point=ceil(numel(c2o_params.non_trains)*0.5);
-test_Iter_size=numel(c2o_params.non_trains)- c2o_params.test_start_point;
-false_match_threshold_final_response=zeros(1,test_Iter_size);
-alfa_min_array=zeros(1,test_Iter_size);
+%define k/10 as the precentage of duplicate samples
+%ex ; k=2 means 20 humans have been re appeared to the camera from the total of 100 inputs
+c2o_params.two_folder_point=200;
+c2o_params.input_sample_size=100;
+
+%for k=1:4
+    
+    k=2;
+    c2o_params.duplicate_feed_size=(c2o_params.input_sample_size*k)/10;
+    
+    %set starting points
+    c2o_params.idxtest_gallery=c2o_params.non_trains(1:c2o_params.two_folder_point);
+    c2o_params.input_array_unshuffled=c2o_params.non_trains(c2o_params.two_folder_point+1:c2o_params.two_folder_point+c2o_params.input_sample_size);
+    repeat_feed=c2o_params.input_array_unshuffled(c2o_params.duplicate_feed_size+1:2*c2o_params.duplicate_feed_size); 
+    c2o_params.input_array_unshuffled(1:c2o_params.duplicate_feed_size)=repeat_feed;
+    c2o_params.probe_cam=1;
+    ix = randperm(numel(c2o_params.input_array_unshuffled));
+    c2o_params.input_array = c2o_params.input_array_unshuffled(ix);
+
+    %final_outs
+    respons_false_match=zeros(10,4);
+    respons_re_id=zeros(10,2);
+
+    %test_results
+    TOTAL_INPUT_COUNT=numel(c2o_params.input_array);
 
 
-%calculate false match rejection threshold
+    for threshold=1:10        
+        
+        tic
+        
+        CORRECT_NEW_COUNT=0;
+        FALSE_NEW_COUNT=0;
+        CORRECT_RE_IDENTIFICATION_COUNT=0;
+        FALSE_RE_IDENTIFICATION_COUNT=0;
+        MISSED_TRUE_NEW=0;
+        CORRECT_RE_APPEARANCE_COUNT=0;
 
-%for i=c2o_params.test_start_point:numel(c2o_params.non_trains)-1
-   
-    
-    i=276;
-    
-    %set the 2Folder threshold.any result value larger than this
-    %value is included in the 2Folder
-    c2o_params.test_start_point=257;
-    
-    %define gallery size
-    c2o_params.gallery_count=i;
-    
-    %define gallery images
-    c2o_params.idxtest_gallery=c2o_params.non_trains(1:i);
-    
-    %define probe images
-    c2o_params.idxtest_probe=c2o_params.non_trains(257:276);
-    
-    %results for the probe images
-    [result,alfamin]=KernelXCRC_VIPER(c2o_params,params);   
-    
-    for i=1:numel(c2o_params.idxtest_probe) % For each probe
-         index = find(result(i,:) >c2o_params.test_start_point);
-         if(numel(index)==0)
-             false_match_threshold_final_response(i)=10000;        
-         else
-             false_match_threshold_final_response(i)=min(index);
-         end
-        alfa_min_array(i)=alfamin(i);
+        for i=1:numel(c2o_params.input_array)
+
+        %parameter setup for iteration
+        c2o_params.probe_cam=1;
+        index=find(c2o_params.input_array==c2o_params.input_array(i));
+        if(index(1) ~= i)
+             c2o_params.probe_cam=2;
+        end
+        c2o_params.idxtest_probe=c2o_params.input_array(i);
+
+
+        %evaluate iteration results
+        raw_result=KernelXCRC_VIPER(c2o_params,params,features); 
+        result=raw_result(1:threshold);
+        possible_matches = find(result >c2o_params.two_folder_point);
+        if(numel(possible_matches)==0)
+            matched_id=-1;     
+        else   
+            matched_id=result(possible_matches(1));
+        end
+
+        if(matched_id == -1)
+            %identified as a new person
+            if(c2o_params.probe_cam==2)
+                FALSE_NEW_COUNT=FALSE_NEW_COUNT+1;
+            else
+                CORRECT_NEW_COUNT=CORRECT_NEW_COUNT+1;
+            end
+
+            c2o_params.idxtest_gallery =[c2o_params.idxtest_gallery c2o_params.input_array(i)];
+
+        else
+            if(index(1) ~= i)
+                CORRECT_RE_APPEARANCE_COUNT=CORRECT_RE_APPEARANCE_COUNT+1;
+            else
+                MISSED_TRUE_NEW=MISSED_TRUE_NEW+1;
+            end
+
+            if(index(1) ~= i)
+                if(c2o_params.idxtest_probe==c2o_params.idxtest_gallery(matched_id))
+                    %re-identified as the correct person - TRUE_POSITIVE
+                    %do nothing to gallery
+                    CORRECT_RE_IDENTIFICATION_COUNT=CORRECT_RE_IDENTIFICATION_COUNT+1;
+                else 
+                    %re-identified as a wrong person - FALSE_POSITIVE
+                    %do nothing to gallery           
+                    FALSE_RE_IDENTIFICATION_COUNT=FALSE_RE_IDENTIFICATION_COUNT+1;
+                end       
+            end
+        end
+
+        end
+
+        respons_false_match(threshold,1)= CORRECT_NEW_COUNT;
+        respons_false_match(threshold,2)= FALSE_NEW_COUNT;
+        respons_false_match(threshold,3)= MISSED_TRUE_NEW;
+        respons_false_match(threshold,4)= CORRECT_RE_APPEARANCE_COUNT;
+        respons_re_id(threshold,1)= CORRECT_RE_IDENTIFICATION_COUNT;
+        respons_re_id(threshold,2)= FALSE_RE_IDENTIFICATION_COUNT;
+        
+        toc
+        fprintf('Test completed for threshold = %s \n',num2str(threshold))
     end
 
-    
+    response_precentages=respons_false_match/TOTAL_INPUT_COUNT;
+    save(strcat('respons_false_match_',num2str(k)),'respons_false_match');
+    save(strcat('response_precentages_',num2str(k)),'response_precentages');
+    save(strcat('respons_re_id_',num2str(k)),'respons_re_id');
+
 %end
-
-save('KernalXCRC_2Folder_out','result');
-minimum=min(alfa_min_array)
-%threshold=min(false_match_threshold_final_response);
-%response_final=zeros(numel(c2o_params.non_trains),numel(c2o_params.non_trains));
-
-%%get aggr_results for tests
-% for i=c2o_params.test_start_point:numel(c2o_params.non_trains)
-%     
-%     c2o_params.gallery_count=i;
-%     c2o_params.idxtest_gallery=c2o_params.non_trains(1:i);
-%     c2o_params.idxtest_probe=c2o_params.non_trains(1:i);
-%     result=KernelXCRC_VIPER(c2o_params,params);
-%     for j=1:i
-%         response_final(i-test_Iter_size+1,result(j,:)==j) = response_final(i-test_Iter_size+1,result(j,:)==j) + 1; 
-%     end
-%     
-% end
-
 
 end
